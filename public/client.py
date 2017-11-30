02232f85d30f4122dfe871438487b63e9cacd03f
jonkasinka <a.bulykina@niuitmo.ru>
2017-11-30 14:58:51 +0300

    Update client.py

--- public/client.py
+++ public/client.py
@@ -5,6 +5,6 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 nick = raw_input("Please, enter you nickname: ")
 print "You may now start messaging"
 while 1:
-	text = raw_input("Your message here: ")
-	text = nick + "> " + text
-	sock.sendto(text,('255.255.255.255',12345))
+    text = raw_input("Your message here: ")
+    text = nick + "> " + text
+    sock.sendto(text,('255.255.255.255',12345))
